d3f707da4dda5b2957f7d360985b0b33390b82ec
GOLDER303 <szymonstepien@interia.pl>
2023-08-03 11:08:39 +0200

    Remove database in favor of templates directory

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { CommandToRun, FileToCopy, PrismaClient } from "@prisma/client"
 import { exec } from "child_process"
-import { copyFile, existsSync, mkdirSync } from "fs"
+import { cp, existsSync, mkdirSync, readFileSync, readdirSync } from "fs"
 import inquirer from "inquirer"
 import path from "path"
 import { fileURLToPath } from "url"
@@ -8,33 +7,25 @@ import { fileURLToPath } from "url"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const CWD = process.cwd()
+const TEMPLATES_DIR_PATH = path.join(__dirname, "..", "templates")
+const COMMANDS_TO_RUN_FILE_NAME = "CommandsToRun.txt"
 
-const prisma = new PrismaClient()
-
-const copyFiles = async (filesToCopy: FileToCopy[], destinationPath: string) => {
-    const copyFilePromises = filesToCopy.map((fileToCopy) => {
-        return new Promise((resolve, reject) => {
-            if (fileToCopy.destinationDirectory) {
-                destinationPath = path.join(destinationPath, fileToCopy.destinationDirectory)
-            }
-
-            if (!existsSync(destinationPath)) {
-                mkdirSync(destinationPath)
-            }
-
-            copyFile(path.join(__dirname, fileToCopy.filePath), path.join(destinationPath, fileToCopy.fileName), (error) => {
-                if (error) reject(error)
-                else resolve(null)
-            })
+const copyFiles = async (source: string, destination: string) => {
+    return new Promise((resolve, reject) => {
+        cp(source, destination, { recursive: true, filter: (src, _) => !src.endsWith(COMMANDS_TO_RUN_FILE_NAME) }, (error) => {
+            if (error) reject(error)
+            else resolve(null)
         })
     })
-
-    return Promise.all(copyFilePromises)
 }
 
-const runCommands = (commandsToRun: CommandToRun[], workingDirectory: string) => {
+const runCommands = (commandsToRun: string[], workingDirectory: string) => {
     commandsToRun.forEach((commandToRun) => {
-        exec(commandToRun.command, { cwd: workingDirectory }, (error, _, stderr) => {
+        if (!commandToRun) {
+            return
+        }
+
+        exec(commandToRun, { cwd: workingDirectory }, (error, _, stderr) => {
             if (error) {
                 throw error
             } else if (stderr) {
@@ -44,8 +35,18 @@ const runCommands = (commandsToRun: CommandToRun[], workingDirectory: string) =>
     })
 }
 
+const getCommandsToRun = (sourceFile: string): string[] => {
+    const commandsFileData = readFileSync(sourceFile, "utf-8")
+
+    if (commandsFileData.includes("\r")) {
+        return commandsFileData.split("\r\n")
+    } else {
+        return commandsFileData.split("\n")
+    }
+}
+
 const getProjectDetailsFromUser = async () => {
-    const availableProjectTypes = await prisma.projectType.findMany()
+    const availableProjectTypes = readdirSync(TEMPLATES_DIR_PATH)
 
     const projectDetails: { projectName: string; projectType: string } = await inquirer.prompt([
         {
@@ -67,16 +68,17 @@ const getProjectDetailsFromUser = async () => {
 const main = async () => {
     const projectDetails = await getProjectDetailsFromUser()
 
+    const TEMPLATE_PATH = path.join(TEMPLATES_DIR_PATH, projectDetails.projectType)
     const PROJECT_PATH = CWD + projectDetails.projectName == "." ? "" : projectDetails.projectName
     if (!existsSync(PROJECT_PATH)) {
         mkdirSync(PROJECT_PATH)
     }
 
-    const projectType = await prisma.projectType.findUniqueOrThrow({ where: { name: projectDetails.projectType }, include: { commandsToRun: true, filesToCopy: true } })
-
     try {
-        await copyFiles(projectType.filesToCopy, PROJECT_PATH)
-        runCommands(projectType.commandsToRun, PROJECT_PATH)
+        await copyFiles(TEMPLATE_PATH, PROJECT_PATH)
+
+        const commandsToRun = getCommandsToRun(path.join(TEMPLATE_PATH, COMMANDS_TO_RUN_FILE_NAME))
+        runCommands(commandsToRun, PROJECT_PATH)
     } catch (error) {
         console.error(error)
         process.exit(1)
@@ -84,11 +86,3 @@ const main = async () => {
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (error) => {
-        console.error(error)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
